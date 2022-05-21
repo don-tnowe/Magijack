c9289adad182c3eb9d456fc1346d84808b7db6c8
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2022-05-22 01:58:09 +0300

    Reorganize stored battler data

--- src/classes/battler_data.gd
+++ src/classes/battler_data.gd
@@ -1,12 +1,11 @@
 class_name BattlerData
 extends Resource
 
-export var hp := 60
 export var hpmax := 60
+export var limit := 16
 
 export var damage := 10
-
-export var limit := 16
+export var greed_damage := 10
 
 export(Resource) var deck
 export(Array, Resource) var spells := []
--- src/classes/enemy_battler_data.gd
+++ src/classes/enemy_battler_data.gd
@@ -11,3 +11,18 @@ export var player_draw_response_chance := 1.0
 export var cards_face_up := 2
 # YOUR first X cards drawn are revealed to enemy. The rest can still be seen using vision spells.
 export var player_cards_face_up := 0
+
+# Levels this appears on.
+export var min_level := 0
+export var max_level := 8
+export var recommended_level := 1
+
+# Returns the difficulty it will be on this level. Make sure to present low (< 0) and high (> 0) choices.
+func get_difficulty(on_level):
+	if min_level > on_level:
+		return -999  # Too easy
+
+	if max_level < on_level:
+		return 999  # Too hard
+
+	return recommended_level - on_level
